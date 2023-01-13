bd98248d240fb6a327618436c5d7ddf963c49341
nickfarm27 <nicholasfarm27@gmail.com>
2023-01-13 22:46:38 +0800

    Add dummy data

---
+++ src/data.js
@@ -0,0 +1,134 @@
+const WALLETS_DATA = [
+  {
+    id: 1,
+    name: 'RHB Debit Card',
+    image: 'credit.png',
+    initialBalance: 1000,
+  },
+];
+
+const CATEGORIES_DATA = [
+  {
+    id: 1,
+    name: 'Food',
+    type: 'expense',
+  },
+  {
+    id: 2,
+    name: 'Transport',
+    type: 'expense',
+  },
+  {
+    id: 3,
+    name: 'Entertainment',
+    type: 'expense',
+  },
+  {
+    id: 4,
+    name: 'Shopping',
+    type: 'expense',
+  },
+  {
+    id: 5,
+    name: 'Others',
+    type: 'expense',
+  },
+  {
+    id: 6,
+    name: 'Salary',
+    type: 'income',
+  },
+  {
+    id: 7,
+    name: 'Allowance',
+    type: 'income',
+  },
+  {
+    id: 8,
+    name: 'Others',
+    type: 'income',
+  },
+];
+
+const TRANSACTIONS_DATA = [
+  {
+    id: 1,
+    walletId: 1,
+    categoryId: 1,
+    date: '2021-01-01',
+    description: 'Chicken Rice - lunch',
+    amount: 25.4,
+  },
+  {
+    id: 2,
+    walletId: 1,
+    categoryId: 2,
+    date: '2021-01-01',
+    description: 'Grab to work',
+    amount: 10.0,
+  },
+  {
+    id: 3,
+    walletId: 1,
+    categoryId: 3,
+    date: '2021-01-01',
+    description: 'Movie date',
+    amount: 50.1,
+  },
+  {
+    id: 4,
+    walletId: 1,
+    categoryId: 4,
+    date: '2021-01-01',
+    description: 'Shopping',
+    amount: 100.3,
+  },
+  {
+    id: 5,
+    walletId: 1,
+    categoryId: 5,
+    date: '2021-01-01',
+    description: 'Others',
+    amount: 10.0,
+  },
+  {
+    id: 6,
+    walletId: 1,
+    categoryId: 1,
+    date: '2021-01-02',
+    description: 'McDonalds',
+    amount: 20.1,
+  },
+  {
+    id: 7,
+    walletId: 1,
+    categoryId: 1,
+    date: '2021-01-03',
+    description: 'Nasi Lemak',
+    amount: 20.0,
+  },
+  {
+    id: 8,
+    walletId: 1,
+    categoryId: 1,
+    date: '2021-01-04',
+    description: 'Chicken Rice',
+    amount: 25.4,
+  },
+  {
+    id: 9,
+    walletId: 1,
+    categoryId: 1,
+    date: '2021-01-05',
+    description: 'Nasi Briyani',
+    amount: 20.0,
+  },
+  {
+    id: 10,
+    walletId: 1,
+    categoryId: 1,
+    date: '2021-01-06',
+    description: 'Chicken Rice',
+    amount: 25.4,
+  },
+];
