0c65acd5fbe3037cf6ef6d3c09fe11ce2d780570
Daniel Yuen <yuendaniel33@gmail.com>
2023-01-14 23:07:40 +0800

    finshed piggy bank

--- src/pages/piggyBank/piggyBank.js
+++ src/pages/piggyBank/piggyBank.js
@@ -16,6 +16,8 @@ import {
   SimpleGrid,
   CardHeader,
   CardBody,
+  Spinner,
+  Text,
 } from '@chakra-ui/react';
 import supabase from '../../supabaseClient';
 
@@ -28,84 +30,150 @@ const PiggyBank = () => {
 
   const [selectedPiggyBank, setSelectedPiggyBank] = useState();
 
+  const PIGGYBANK_OPTIONS = [
+    {
+      name: 'Retirement Fund',
+      desc: 'Funds for retiring comfortably',
+      image:
+        'https://images.unsplash.com/photo-1473186578172-c141e6798cf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
+    },
+    {
+      name: 'Education Fund',
+      desc: "Ensure your children's future education",
+      image: 'https://www.wilkes.edu/_images/wilkes-university.jpg',
+    },
+    {
+      name: 'Investments',
+      desc: 'Grow your wealth by setting aside funds for investing',
+      image:
+        // 'https://ocdn.eu/images/pulscms/NmE7MDA_/9800cac2d167fa9f826e41c7b25b968e.jpg',
+        'https://i.kym-cdn.com/photos/images/newsfeed/001/499/826/2f0.png',
+    },
+    {
+      name: 'Custom Piggy Bank',
+      desc: 'Create your own custom goals',
+      image:
+        'https://kdvr.com/wp-content/uploads/sites/11/2022/03/best-piggy-bank-16f62d.jpg',
+    },
+  ];
+
   const onClickHandler = index => {
     setSelectedItem(index);
   };
 
   useEffect(() => {
-    supabase
-      .from('piggybank')
-      .select('name, desc, total, paid, per_month')
-      .then(res => setItems(res.data));
+    const getPiggybank = async () => {
+      await supabase
+        .from('piggybank')
+        .select('name, desc, total, paid, per_month')
+        .then(res => setItems(res.data));
+    };
+    getPiggybank();
   }, []);
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Layout>
-      <Flex gap="30px" direction="row">
-        <Flex gap="25px" direction="column" w="50%">
-          {items &&
-            items.map((item, index) => (
-              <ItemCard
-                key={index}
-                index={index}
-                title={item.name}
-                desc={item.desc}
-                total={item.total}
-                paid={item.paid}
-                onClick={onClickHandler}
-              />
-            ))}
-        </Flex>
-        <Flex gap="20px" direction="column" w="100%">
-          <Box>
-            <Button
-              variant="primaryButton"
-              w="auto"
-              _hover={{ transform: '' }}
-              float="right"
-              onClick={onOpen}
-            >
-              {' '}
-              Add Piggy Bank
-            </Button>
-          </Box>
-          {items && <ItemDetails selectedItem={items[selectedItem]} />}
-        </Flex>
-      </Flex>
+      {items ? (
+        <>
+          <Flex gap="30px" direction="row">
+            <Flex gap="25px" direction="column" w="50%">
+              {items &&
+                items.map((item, index) => (
+                  <ItemCard
+                    key={index}
+                    index={index}
+                    title={item.name}
+                    desc={item.desc}
+                    total={item.total}
+                    paid={item.paid}
+                    onClick={onClickHandler}
+                  />
+                ))}
+            </Flex>
+            <Flex gap="20px" direction="column" w="100%">
+              <Box>
+                <Button
+                  variant="primaryButton"
+                  w="auto"
+                  _hover={{ transform: '' }}
+                  float="right"
+                  onClick={onOpen}
+                >
+                  {' '}
+                  Add Piggy Bank
+                </Button>
+              </Box>
+              {items && <ItemDetails selectedItem={items[selectedItem]} />}
+            </Flex>
+          </Flex>
 
-      <Modal isOpen={isOpen} onClose={onClose} isCentered size="xl">
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Add New Piggy Bank</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <SimpleGrid columns={2} spacing={5}>
-              <Card cursor="pointer" onClick={setSelectedPiggyBank}>
-                <CardHeader fontWeight="bold">Retirement Fund</CardHeader>
-                <CardBody>Funds for retiring comfortably</CardBody>
-              </Card>
-              <Card>
-                <CardHeader fontWeight="bold">Education Fund</CardHeader>
-                <CardBody>Ensure your children's future education</CardBody>
-              </Card>
-              <Card>
-                <CardHeader fontWeight="bold">Investments</CardHeader>
-                <CardBody>
-                  Grow your wealth by setting aside funds for investing
-                </CardBody>
-              </Card>
-              <Card>
-                <CardHeader fontWeight="bold">Custom Piggy Bank</CardHeader>
-                <CardBody>Create your own custom goals</CardBody>
-              </Card>
-            </SimpleGrid>
-          </ModalBody>
-          <ModalFooter>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+          <Modal isOpen={isOpen} onClose={onClose} isCentered size="xl">
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Add New Piggy Bank</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <SimpleGrid columns={2} spacing={5}>
+                  {PIGGYBANK_OPTIONS.map((option, index) => (
+                    <Card
+                      cursor="pointer"
+                      onClick={setSelectedPiggyBank.bind(null, index)}
+                      transform="scale(1)"
+                      _hover={{
+                        transform: 'scale(1.02)',
+                        transitionDuration: '0.2s',
+                      }}
+                    >
+                      <CardHeader fontWeight="bold">
+                        <Box
+                          position="absolute"
+                          left="0"
+                          top="0"
+                          height="70px"
+                          width="100%"
+                          backgroundImage={`url(${option.image})`}
+                          backgroundSize="100%"
+                          objectFit="cover"
+                          backgroundRepeat="no-repeat"
+                          borderRadius="5px 5px 0 0"
+                          zIndex="-1"
+                        />
+                        <Text
+                          fontWeight="bold"
+                          color="white"
+                          textShadow="2px 2px 10px #000000"
+                          fontSize="20px"
+                        >
+                          {option.name}
+                        </Text>
+                      </CardHeader>
+                      <CardBody>{option.desc}</CardBody>
+                    </Card>
+                  ))}
+                </SimpleGrid>
+              </ModalBody>
+              <ModalFooter></ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
+      ) : (
+        <Flex
+          w="100%"
+          marginTop="30vh"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Spinner
+            size="xl"
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="secondaryBlue"
+          />
+        </Flex>
+      )}
     </Layout>
   );
 };
